22570b3612d5020b63b80af0d744630438657656
Michele Piccirillo <michele.piccirillo@gmail.com>
2020-02-08 17:46:25 +0100

    Drop user privileges to be runner:docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
 FROM alpine/git
+
 COPY github.sig /github.sig
 COPY entrypoint.sh /entrypoint.sh
+
+RUN addgroup docker
+RUN adduser -D -g '' runner docker
+RUN mkdir /github/workspace  && chown runner:docker /github/workspace
+
+WORKDIR /github/workspace
+VOLUME /github/workspace
+
+USER runner
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,16 @@ sig=`cat /github.sig`
 challenge=`ssh-keyscan -t rsa github.com 2>/dev/null | ssh-keygen -lf -`
 
 if [ "$challenge" = "$sig" ]; then
-    mkdir /root/.ssh/
-    chmod 700 /root/.ssh/
+    mkdir ~/.ssh/
+    chmod 700 ~/.ssh/
     ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
     if [ "$1" = "" ]; then
         echo "No SSH key detected, attempting public checkout"
         git submodule update --init --recursive
     else
         echo "SSH key detected, attempting private checkout"
-        echo "$1" > /root/.ssh/ssh.key
-        chmod 600 /root/.ssh/ssh.key
+        echo "$1" > ~/.ssh/ssh.key
+        chmod 600 ~/.ssh/ssh.key
         export GIT_SSH_COMMAND="ssh -i /root/.ssh/ssh.key"
         git submodule update --init --recursive
     fi;
