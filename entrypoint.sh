4bdc296255cc7ddb06e7b0eb2f4f74343ac70b62
Michele Piccirillo <michele.piccirillo@gmail.com>
2020-02-08 18:00:24 +0100

    Use $HOME

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,23 @@
 sig=`cat /github.sig`
 challenge=`ssh-keyscan -t rsa github.com 2>/dev/null | ssh-keygen -lf -`
 
+echo "Working as user $(whoami) [$HOME]"
+
 if [ "$challenge" = "$sig" ]; then
-    mkdir ~/.ssh/
-    chmod 700 ~/.ssh/
-    ssh-keyscan github.com 2>/dev/null >> ~/.ssh/known_hosts
+    mkdir "$HOME/.ssh/"
+    chmod 700 "$HOME/.ssh/"
+    ssh-keyscan github.com 2>/dev/null >> "$HOME/.ssh/known_hosts"
+
     if [ "$1" = "" ]; then
         echo "No SSH key detected, attempting public checkout"
-        git submodule update --init --recursive
     else
         echo "SSH key detected, attempting private checkout"
-        echo "$1" > ~/.ssh/ssh.key
-        chmod 600 ~/.ssh/ssh.key
+        echo "$1" > "$HOME/.ssh/ssh.key"
+        chmod 600 "$HOME/.ssh/ssh.key"
         export GIT_SSH_COMMAND="ssh -i ~/.ssh/ssh.key"
-        git submodule update --init --recursive
     fi;
+
+    git submodule update --init --recursive
 else
     echo "Signature validation failed!"
     exit 1
