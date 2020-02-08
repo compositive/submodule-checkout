d122652f7934aa5854ff7c47ae7fda0ba38f95d3
Michele Piccirillo <michele.piccirillo@gmail.com>
2020-02-08 17:53:29 +0100

    Fix references to the /root home

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ challenge=`ssh-keyscan -t rsa github.com 2>/dev/null | ssh-keygen -lf -`
 if [ "$challenge" = "$sig" ]; then
     mkdir ~/.ssh/
     chmod 700 ~/.ssh/
-    ssh-keyscan github.com 2>/dev/null >> /root/.ssh/known_hosts
+    ssh-keyscan github.com 2>/dev/null >> ~/.ssh/known_hosts
     if [ "$1" = "" ]; then
         echo "No SSH key detected, attempting public checkout"
         git submodule update --init --recursive
@@ -14,7 +14,7 @@ if [ "$challenge" = "$sig" ]; then
         echo "SSH key detected, attempting private checkout"
         echo "$1" > ~/.ssh/ssh.key
         chmod 600 ~/.ssh/ssh.key
-        export GIT_SSH_COMMAND="ssh -i /root/.ssh/ssh.key"
+        export GIT_SSH_COMMAND="ssh -i ~/.ssh/ssh.key"
         git submodule update --init --recursive
     fi;
 else
